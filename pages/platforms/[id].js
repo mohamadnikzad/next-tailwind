0243e0f93942081ab6285320a4b25382b240da7c
Dante <mrdante1616@gmail.com>
2021-08-07 20:03:38 +0430

    change statuc filles

--- pages/platforms/[id].js
+++ pages/platforms/[id].js
@@ -1,41 +1,38 @@
-import Head from "next/head"
-import Header from "../../components/layout/Header"
-import PageContent from "../../components/PageContent"
-
+import Head from "next/head";
+import Header from "../../components/layout/Header";
+import PageContent from "../../components/PageContent";
 
 const Platform = ({ platform }) => {
-    const gamesList = platform.results
-    const nextPage = platform.next
-    return (
-        <>
-            <Head>
-                <title>NZ</title>
-                <link rel="icon" href="/favicon.ico" />
-            </Head>
-            <Header />
-            <PageContent gamesList={gamesList} nextPage={nextPage} pageTitle='by platform' />
-        </>
-    )
-}
+  const gamesList = platform.results;
+  const nextPage = platform.next;
+  return (
+    <>
+      <Head>
+        <title>NZ</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Header />
+      <PageContent
+        gamesList={gamesList}
+        nextPage={nextPage}
+        pageTitle="by platform"
+      />
+    </>
+  );
+};
 
-export default Platform
+export default Platform;
 
 export function getStaticPaths() {
-    return {
-        paths: [
-            { params: { id: '1' } },
-            { params: { id: '2' } },
-            { params: { id: '3' } },
-            { params: { id: '4' } },
-            { params: { id: '8' } },
-        ],
-        fallback: false //faslse  any other route will return 404
-    };
+  return {
+    paths: [{ params: { id: "1" } }],
+    fallback: true, //faslse  any other route will return 404
+  };
 }
 export const getStaticProps = async ({ params }) => {
-    const res = await fetch(`https://api.rawg.io/api/games?parent_platforms=${params.id}&key=${process.env.API_KEY}`)
-    const platform = await res.json()
-    return { props: { platform } }
-}
-
-
+  const res = await fetch(
+    `https://api.rawg.io/api/games?parent_platforms=${params.id}&key=${process.env.API_KEY}`
+  );
+  const platform = await res.json();
+  return { props: { platform } };
+};
--- pages/popular-in-2020.js
+++ pages/popular-in-2020.js
@@ -1,28 +1,33 @@
-import Header from "../components/layout/Header"
-import PageContent from "../components/PageContent"
-import Head from 'next/head'
+import Header from "../components/layout/Header";
+import PageContent from "../components/PageContent";
+import Head from "next/head";
 
-
-const PopularIn2020 = ({ data }) => {
-    const gamesList = data.results
-    const nextPage = data.next
-    return (
-        <>
-            <Head>
-                <title>Nikzad</title>
-                <link rel="icon" href="/favicon.ico" />
-            </Head>
-            <Header />
-            <PageContent gamesList={gamesList} nextPage={nextPage} pageTitle='Popular in 2020' />
-        </>
-    )
-}
+const PopularIn2020 = ({ data, url }) => {
+  const gamesList = data.results;
+  const nextPage = data.next;
+  //   console.log(url);
+  return (
+    <>
+      <Head>
+        <title>Nikzad</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Header />
+      <PageContent
+        gamesList={gamesList}
+        nextPage={nextPage}
+        pageTitle="Popular in 2020"
+      />
+    </>
+  );
+};
 export const getStaticProps = async () => {
-    const res = await fetch(`https://rawg.io/api/games/lists/greatest?year=2020&discover=true&ordering=-added&page_size=20&page=1&key=${process.env.API_KEY}`)
-    const data = await res.json()
+  const url = `https://rawg.io/api/games/lists/greatest?year=2020&discover=true&ordering=-added&page_size=20&page=1&key=${process.env.API_KEY}`;
+  const res = await fetch(url);
+  const data = await res.json();
 
-    return {
-        props: { data }
-    }
-}
-export default PopularIn2020
+  return {
+    props: { data, url },
+  };
+};
+export default PopularIn2020;
